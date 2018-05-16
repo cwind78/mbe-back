f14917fda2808db649791d6a26a3554b56b3dd7c
kwangsu.kim <cwind78@naver.com>
2018-05-16 09:04:31 +0900

    extra update

---
+++ mbe/src/main/java/com/org/app/main/vo/Recommand_Denial.java
@@ -0,0 +1,54 @@
+package com.org.app.main.vo;
+
+public class Recommand_Denial {
+	private String user_id;
+	private String denial_user_id;
+	private String use_yn;
+	private String regist_date;
+	private String regist_user_id;
+	private String update_date;
+	private String update_user_id;
+	
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getDenial_user_id() {
+		return denial_user_id;
+	}
+	public void setDenial_user_id(String denial_user_id) {
+		this.denial_user_id = denial_user_id;
+	}
+	public String getUse_yn() {
+		return use_yn;
+	}
+	public void setUse_yn(String use_yn) {
+		this.use_yn = use_yn;
+	}
+	public String getRegist_date() {
+		return regist_date;
+	}
+	public void setRegist_date(String regist_date) {
+		this.regist_date = regist_date;
+	}
+	public String getRegist_user_id() {
+		return regist_user_id;
+	}
+	public void setRegist_user_id(String regist_user_id) {
+		this.regist_user_id = regist_user_id;
+	}
+	public String getUpdate_date() {
+		return update_date;
+	}
+	public void setUpdate_date(String update_date) {
+		this.update_date = update_date;
+	}
+	public String getUpdate_user_id() {
+		return update_user_id;
+	}
+	public void setUpdate_user_id(String update_user_id) {
+		this.update_user_id = update_user_id;
+	}
+}
---
+++ mbe/src/main/java/com/org/app/main/vo/Recommand_User.java
@@ -0,0 +1,131 @@
+package com.org.app.main.vo;
+
+public class Recommand_User {
+	private String seq_no;
+	private String user_id;
+	private String recommand_user_id;
+	private String recommand_status;
+	private String call_datetime;
+	private String caution_yn;
+	private String regist_date;
+	private String regist_user_id;
+	private String update_date;
+	private String update_user_id;
+	private String gender;
+	private String age;
+	private String hobby;
+	private String job;
+	private String area;
+	private String purpose;
+	private String photo;
+	private String user_name;
+	
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public String getSeq_no() {
+		return seq_no;
+	}
+	public void setSeq_no(String seq_no) {
+		this.seq_no = seq_no;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getRecommand_user_id() {
+		return recommand_user_id;
+	}
+	public void setRecommand_user_id(String recommand_user_id) {
+		this.recommand_user_id = recommand_user_id;
+	}
+	public String getRecommand_status() {
+		return recommand_status;
+	}
+	public void setRecommand_status(String recommand_status) {
+		this.recommand_status = recommand_status;
+	}
+	public String getCall_datetime() {
+		return call_datetime;
+	}
+	public void setCall_datetime(String call_datetime) {
+		this.call_datetime = call_datetime;
+	}
+	public String getCaution_yn() {
+		return caution_yn;
+	}
+	public void setCaution_yn(String caution_yn) {
+		this.caution_yn = caution_yn;
+	}
+	public String getRegist_date() {
+		return regist_date;
+	}
+	public void setRegist_date(String regist_date) {
+		this.regist_date = regist_date;
+	}
+	public String getRegist_user_id() {
+		return regist_user_id;
+	}
+	public void setRegist_user_id(String regist_user_id) {
+		this.regist_user_id = regist_user_id;
+	}
+	public String getUpdate_date() {
+		return update_date;
+	}
+	public void setUpdate_date(String update_date) {
+		this.update_date = update_date;
+	}
+	public String getUpdate_user_id() {
+		return update_user_id;
+	}
+	public void setUpdate_user_id(String update_user_id) {
+		this.update_user_id = update_user_id;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getHobby() {
+		return hobby;
+	}
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public String getPurpose() {
+		return purpose;
+	}
+	public void setPurpose(String purpose) {
+		this.purpose = purpose;
+	}
+}
